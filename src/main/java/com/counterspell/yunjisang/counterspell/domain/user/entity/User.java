13a5ae026361b69945f5c4ced9589199af2f949c
balhyo-younjisang <yjs12180825@gmail.com>
2024-11-23 13:54:51 +0900

    feat: user Entity 추가

---
+++ src/main/java/com/counterspell/yunjisang/counterspell/domain/user/entity/User.java
@@ -0,0 +1,84 @@
+package com.counterspell.yunjisang.counterspell.domain.user.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "이메일 형식이 올바르지 않습니다")
+    @Comment("사용자 이메일")
+    private String email;
+
+    @Column(nullable = false)
+    @Comment("사용자 비밀번호")
+    private String password;
+
+    @Column(nullable = false)
+    @Comment("사용자 이름")
+    private String username;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Comment("사용자 권한")
+    private Role role;
+
+    public void setEmail(@Email(message = "이메일 형식이 올바르지 않습니다") String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Builder
+    public User(String email, String password, String username, Role role) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
