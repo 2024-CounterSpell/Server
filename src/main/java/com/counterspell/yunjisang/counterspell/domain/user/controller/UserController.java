8588ab99f1a0be56e649fd3c674dda46ca46f4ef
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-23 14:46:54 +0900

    FIX: ApiPath
    API 요청 경로 설정 클래스를 생성하여 컨트롤러에 적용하였습니다

--- src/main/java/com/counterspell/yunjisang/counterspell/domain/user/controller/UserController.java
+++ src/main/java/com/counterspell/yunjisang/counterspell/domain/user/controller/UserController.java
@@ -3,11 +3,14 @@ package com.counterspell.yunjisang.counterspell.domain.user.controller;
 import com.counterspell.yunjisang.counterspell.domain.user.dto.RequestUserDTO;
 import com.counterspell.yunjisang.counterspell.domain.user.dto.ResponseUserDTO;
 import com.counterspell.yunjisang.counterspell.domain.user.service.UserService;
+import com.counterspell.yunjisang.counterspell.global.common.ApiPath;
 import com.counterspell.yunjisang.counterspell.global.common.DefaultController;
 import com.counterspell.yunjisang.counterspell.global.common.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping(ApiPath.USER_API_PATH)
 public class UserController extends DefaultController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public ResponseEntity<SuccessResponse<ResponseUserDTO.RegisterUserDTO>> registerUser(@RequestBody RequestUserDTO.RegisterUserDTO registerUserDTO) {
-        ResponseUserDTO.RegisterUserDTO dto = userService.save(registerUserDTO.toEntity());
+    public ResponseEntity<SuccessResponse<ResponseUserDTO.RegisterUserDTO>> registerUser(@Valid @RequestBody RequestUserDTO.RegisterUserDTO registerUserDTO) {
+        ResponseUserDTO.RegisterUserDTO dto = userService.save(registerUserDTO.toEntity(passwordEncoder));
         SuccessResponse<ResponseUserDTO.RegisterUserDTO> response = new SuccessResponse<>(true, dto);
 
         return new ResponseEntity<>(response, createHttpHeaders(), HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<SuccessResponse<ResponseUserDTO.CreateJWTDTO>> createJWT(@RequestBody RequestUserDTO.CreateJWTDTO createJWTDTO) {
+    public ResponseEntity<SuccessResponse<ResponseUserDTO.CreateJWTDTO>> createJWT(@Valid @RequestBody RequestUserDTO.CreateJWTDTO createJWTDTO) {
         ResponseUserDTO.CreateJWTDTO dto = userService.createJWT(createJWTDTO);
         SuccessResponse<ResponseUserDTO.CreateJWTDTO> response = new SuccessResponse<>(true, dto);
 
