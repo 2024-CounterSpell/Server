2aaba6c48be04490270e9cbea9cf4c01e4525369
balhyo-younjisang <yjs12180825@gmail.com>
2024-11-23 13:44:07 +0900

    Initialized Repository

---
+++ src/main/java/com/counterspell/yunjisang/counterspell/CounterspellApplication.java
@@ -0,0 +1,13 @@
+package com.counterspell.yunjisang.counterspell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterspellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounterspellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/counterspell/yunjisang/counterspell/CounterspellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counterspell.yunjisang.counterspell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterspellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
