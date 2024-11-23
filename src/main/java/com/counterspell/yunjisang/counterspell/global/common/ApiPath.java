48f6baf6b8f2c65246bfc07e21463bd1c46a0f0b
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-23 19:01:42 +0900

    FIX: ApiPath

--- src/main/java/com/counterspell/yunjisang/counterspell/global/common/ApiPath.java
+++ src/main/java/com/counterspell/yunjisang/counterspell/global/common/ApiPath.java
@@ -4,5 +4,5 @@ public class ApiPath {
     public static final String BASE_PATH = "/api/v1";
     public static final String USER_API_PATH = BASE_PATH + "/user";
     public static final String ADMIN_API_PATH = BASE_PATH + "/admin";
-    public static final String CONVERSATION_API_PATH = BASE_PATH + "/conversation";
+    public static final String CONVERSATION_API_PATH = BASE_PATH + "/ai/conversation";
 }
