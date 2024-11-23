f4e9b5452217035caa04cd8e073f33d097c55f70
hyeonjoonpark <pjjoon1379@gmail.com>
2024-11-23 19:00:31 +0900

    FEAT: Video
    Video에 저장 날짜 컬럼 생성

--- src/main/java/com/counterspell/yunjisang/counterspell/domain/video/entity/Video.java
+++ src/main/java/com/counterspell/yunjisang/counterspell/domain/video/entity/Video.java
@@ -4,10 +4,14 @@ import com.counterspell.yunjisang.counterspell.domain.user.entity.User;
 import com.counterspell.yunjisang.counterspell.domain.word.Word;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Entity
-@Getter
+import java.time.LocalDateTime;
+
+@Entity @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Video {
     @Id @Column(name = "video_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,11 +25,14 @@ public class Video {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
+    @CreatedDate private LocalDateTime createdAt;
+
     @Builder
-    public Video(String videoName, String videoPath, Word words, User user) {
+    public Video(String videoName, String videoPath, Word words, User user, LocalDateTime createdAt) {
         this.videoName = videoName;
         this.videoPath = videoPath;
         this.words = words;
         this.user = user;
+        this.createdAt = createdAt;
     }
 }
